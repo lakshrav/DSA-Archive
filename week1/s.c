31eddf12717cf0142e0e67d109409db69062ae65
Lakshana <lakshravi23@gmail.com>
2021-12-12 09:53:41 +0530

    half done

--- week1/s.c
+++ week1/s.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 
+void dijkstra(int n, int G[][n], int source, int t[][n-1]) 
+{
+	
 int main ()
 {
-	printf("h");
+	int G[][] = {
+		{0, 0, 0, 0, 0, 0},
+		{0, 0, 10, 0, 30, 100},
+		{0, 0, 0, 50, 0, 0},
+		{0, 0, 0, 0, 0, 10},
+		{0, 0, 0, 20, 0, 60},
+		{0, 0, 0, 0, 0, 0}};
+	int source = 1;
+	int t[2][5];
+	
+	dijkstra(6, G, source, t);
+	int i;
+	for (i = 0; i < 5; ++i)  {
+		printf("%d %d\n", t[0][i], t[1][i]);
+	}
 	return 0;
 }
+		
