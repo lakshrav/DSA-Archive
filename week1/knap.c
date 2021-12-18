34128a68d06749c25087c3d4f74bc755f89825d6
Lakshana <lakshravi23@gmail.com>
2021-12-18 21:46:04 +0530

    knapsack half done

---
+++ week1/knap.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#define SIZE 100
+
+void merge(float a[], int l, int mid, int h)
+{
+	float b[SIZE];
+	int i, j, k;
+	i = l;
+	k = l;
+	j = mid;
+
+	while (i <= mid && j <= h)  {
+		if (a[i] < a[j])  
+			b[k++] = a[i++];
+		else 
+			b[k++] = a[j++];
+	}
+	for (; i <= mid; ++i)  
+		b[k++] = a[i];
+
+	for (; j <= h; ++j)  
+		b[k++] = a[j];
+	for (i = l; i <= h; ++i)  
+		a[i] = b[i];
+}
+	
+void mergesort(float a[],int l, int h)
+{
+	if (l < h)  {
+		int mid = (l + h)/2;
+		mergesort(a, l, mid);
+		mergesort(a,  mid+1, h);
+		merge(a, l, mid, h);
+	}
+}
+		
+int main ()
+{
+	int n;
+	printf("Enter number of entries: ");
+	scanf("%d", &n);
+	int i, w[n], p[n]; 
+	float pw[n];
+	printf("Enter the weights of the objects: ");
+	
+	for (i = 0; i < n; ++i)  {
+		scanf("%d", &w[i]);
+	}
+
+	printf("Enter the profits of the objects: ");
+	
+	for (i = 0; i < n; ++i)  {
+		scanf("%d", &p[i]);
+	}
+	for (i = 0; i < n; ++i)  {
+		pw[i] = (float) p[i] / w[i];
+	}
+	//sorting the pw array
+	for (i = 0; i < n; ++i)  
+		printf("%.2f\n",pw[i]); 
+	int j;
+	mergesort(pw,0,n-1);
+	for (i = 0; i < n; ++i)  
+		printf("%.2f\n",pw[i]); 
+}
+
+
+	 
+	
