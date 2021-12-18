5f2b8f0b0bb9afcddc035457075840995e61a1f7
Lakshana <lakshravi23@gmail.com>
2021-12-18 16:26:06 +0530

    cf

--- week1/cfkad.c
+++ week1/cfkad.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
 #include <string.h>
+#include <limits.h>
 
+int max(int a, int b)
+{
+	return a > b? a:b;
+}
 int main ()
 {
 	char st[100];
 	scanf("%s%*c", st);
 	int n = strlen(st);
 	
-	int i, total, current, prev;
-	total = 0;
-	prev = current = st[0];
+	int i, curr, total, prev;
+	curr = 0, total = INT_MIN;
+
+	prev = st[0];
 	for (i = 0; i < n; ++i)  {
-		current = st[i];
-		if (prev == current)  
-			total++;
+		if (prev == st[i])  
+			curr++;
 		else 
-			total = 1;
-		prev = current;
+			curr = 1;
+		prev = st[i];
+		total = max(total , curr);
 	}
 	if (total >= 7)  
 		printf("YES");
