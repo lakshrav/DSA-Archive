4369c81df222f059cd8fd2261781e9c3cb1c1a6b
Lakshana <lakshravi23@gmail.com>
2021-12-18 16:49:14 +0530

    kruskal hardcode

--- week1/kruskal.c
+++ week1/kruskal.c
@@ -2,14 +2,14 @@
 #include <limits.h>
 #define I INT_MAX
 
-int edges[3] = {
+int edges[3][9] = {
 	{1, 1, 2, 2, 3, 4, 4, 5, 5},
 	{2, 6, 3, 7, 4, 5, 7, 6, 7},
 	{25, 5, 12, 10, 8, 16, 14, 20, 18}};
 	
 int set[8] = {-1};
 int included[9] ={0};
-int t[2]][6];
+int t[2][6];
 	
 
 void join(int x, int y)
@@ -41,4 +41,24 @@ int par(int x)
 
 int main()
 {
-	int 
+	int i = 0, j, k, u, v, min = I, n = 7, e = 9;
+	while (i < n - 1)  {
+		min = I;
+		for (j = 0; j < e; ++j)  {
+			if (included[j] == 0 && edges[2][j] < min)  {
+				min = edges[2][j];
+				u = edges[0][j], v = edges[1][j];
+				k = j;
+			}
+		}
+		if (par(u) != par(v))  {
+			t[0][i] = u, t[1][i] = v;
+			join(par(u), par(v));
+			i++;
+		}
+		included[k] = 1;
+	}
+	for (i = 0; i < n - 1; ++i) 
+		printf("%d %d\n", t[0][i], t[1][i]);
+	return 0;
+} 
