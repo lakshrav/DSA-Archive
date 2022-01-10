ee16716276ca989b4c20b779d09336b4d82cf845
Lakshana <lakshravi23@gmail.com>
2022-01-10 13:56:40 +0530

    LCS complete

--- week1/lcs.c
+++ week1/lcs.c
@@ -31,17 +31,32 @@ void find_lcs(char a[], char b[])
 				lcs[i][j] = 0;
 			else if (a[i - 1] == b[j - 1]) {
 				lcs[i][j] = lcs[i - 1][ j - 1] + 1;
-				final[k++] = a[i - 1];
 			}
 			else 
 				lcs[i][j] = max(lcs[i - 1][j], lcs[i][j - 1]);
 		}
 	}
-	final[k] = '\0';
-	printf("The LCS is of length %d: ", k);
-	
-	for (i = 0; i < k; ++i)  
+	i = m, j = n;
+	int num = lcs[m][n];
+	while (i > 0)  {
+		while (j > 0)  {
+			if (lcs[i-1][j-1] == lcs[i][j] - 1)  {
+				final[k++] = b[j-1];	
+				i--, j--;
+				if (!lcs[i][j])
+					break;
+			}
+			else  
+				j--;
+		}
+		if (lcs[i][j] == 0)
+			break;
+		i--, j = n;
+	}
+	final[k] = '\0'; 
+	for (i = k - 1; i >= 0 ; --i)  
 		printf("%c", final[i]);
+	printf("\n");
 }
 
 int main ()
